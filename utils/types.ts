3730e0ae91317a6ba031b5c0cb8637d97abea59d
Bhavy <bhavy2908@gmail.com>
2024-08-29 22:39:36 +0530

    Update types.ts

--- utils/types.ts
+++ utils/types.ts
@@ -1,1 +1,19 @@
-export type Query = { type: string; query: string };
+import { Node, Edge } from "reactflow";
+
+export interface Query {
+  type: string;
+  query: string;
+}
+
+export interface ExtendedNode extends Node {
+  data: {
+    label: string;
+    isExpanded: boolean;
+    depth: number;
+    isTypeNode?: boolean;
+    parentId?: string;
+    type?: string;
+    isCrowned?: boolean;
+    fact?: string;
+  };
+}
